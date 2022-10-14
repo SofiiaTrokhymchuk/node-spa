46e1ff79bf1a81abd53c5eb9627a67108765b3ff
SofiiaTrokhymchuk <s.s.trokhymchuk@gmail.com>
2022-10-14 22:35:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node.js SPA
+
+This is a simple SPA using Node.js
---
+++ server.js
@@ -0,0 +1,16 @@
+const PORT = 3000;
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+});
